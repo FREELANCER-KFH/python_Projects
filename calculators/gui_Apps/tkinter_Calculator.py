8e51f698deccc275799a4a427708b6892e4443a3
programmerKFH <kfh.freelancer@gmail.com>
2024-01-06 23:51:59 -0400

    Fixed btn_click function and updated the UI.

--- calculators/gui_Apps/tkinter_Calculator.py
+++ calculators/gui_Apps/tkinter_Calculator.py
@@ -12,6 +12,35 @@
 #Import
 from tkinter import *
 
+#functions
+def btn_click(value):
+    if value == "=":
+        try:
+            result = eval(txt_Entry.get())
+            txt_Entry.delete(0, END)
+            txt_Entry.insert(0, result)
+        except:
+            txt_Entry.delete(0, END)
+            txt_Entry.insert(0, "Error")
+    elif value == "C":
+        txt_Entry.delete(0, END)
+    elif value == "√":
+        try:
+            result = eval(txt_Entry.get())
+            txt_Entry.delete(0, END)
+            txt_Entry.insert(0, result ** 0.5)
+        except:
+            txt_Entry.delete(0, END)
+            txt_Entry.insert(0, "Error")
+    else:
+        txt_Entry.insert(END, value)
+
+def txt_show(value):
+    if value == 0:
+        return "0"
+    else:
+        return value
+
 #Window
 root = Tk()
 root.title("Calculator")
@@ -31,7 +60,7 @@ btn_list = [
 
 #Buttons
 def btn_create(value):
-    btn = Button(root, text=value, font=("arial", 20, "bold"), width=5, height=2, command=lambda:click_btn(value))
+    btn = Button(root, text=value, font=("arial", 20, "bold"), width=5, height=2, borderwidth=5, background="black", fg="white", command=lambda: btn_click(value))
     return btn
 
 #Grid
@@ -42,99 +71,9 @@ for row in range(1, 6):
         btn.grid(row=row, column=column, padx=5, pady=5)
         i += 1
 
-#Variables
-    operator = ""
-    number = ""
-    text = StringVar()
-    container = ""
-    result = 0
-
-#Click
-def click_btn(value):
-    global number
-    global operator
-    global container
-    text = str(value)
-    if text.isnumeric():
-        text = int(text)
-        if (text >= 0 and text <= 9):
-            number += str(text)
-            container = number
-            update(number)
-    elif text == '.' or text == '(' or text == ')':
-        number += text
-        update(number)
-    else:
-        if text == '+' or text == '-' or text == '*' or text == '/' or text == '%':
-            operator = text
-            update(operator)
-            number = ""
-        elif text == '=':
-            update(calculate(int(container), operator))
-            number = ""
-            operator = ""
-        elif text == 'C':
-            clear()
-
-#Calculate
-def calculate(current, op):
-    global result
-    match(op):
-        case '+':
-            result += current
-            return result
-        case '-':
-            result -= current
-            return result
-        case '*':
-            result *= current
-            return result
-        case '/':
-            result /= current
-            return result
-        case '%':
-            result %= current
-            return result
-
-#Clear
-def clear():
-    global operator
-    global result
-    global number
-    number = ""
-    operator = ""
-    result = 0
-    update(number)
-
-#Update
-def update(value):
-    text.set(value)
-
-#Equal
-def equal():
-    global operator
-    try:
-        result = eval(operator)
-        operator = str(result)
-        text.set(result)
-    except:
-        text.set("Error")
-        operator = ""
-
-#Square root
-def square_root():
-    global operator
-    try:
-        result = eval(operator)
-        operator = str(result)
-        text.set(result)
-    except:
-        text.set("Error")
-        operator = ""
-
 #Screen
-screen = Entry(root, font=("arial", 20, "bold"), width=22, borderwidth=10, background="black", fg="white", textvariable=text)
-screen.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+txt_Entry = Entry(root, font=("arial", 20, "bold"), width=22, borderwidth=10, background="black", fg="white", textvariable=txt_show(0))
+txt_Entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
 #Loop
 root.mainloop()
